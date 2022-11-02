8aeca900ec2aa7045706934102c0fbc91e67a8d1
Antonio Coppe <antonio.coppe@gmail.com>
2022-11-02 11:57:10 +0100

    testing in main

--- Program.cs
+++ Program.cs
@@ -2,10 +2,22 @@
 
 class Program{
     static int Main(string[] args){
-        Console.WriteLine("Hello world!!");
-        for (var n = 0; n < args.Length; n++){
-            Console.WriteLine("arg[{0}] = {1}",n,args[n]);
+        
+        //This are the my first C# lines of code
+        // Console.WriteLine("Hello world!!");
+        // for (var n = 0; n < args.Length; n++){
+        //     Console.WriteLine("arg[{0}] = {1}",n,args[n]);
+        // }
+        // return 0;
+        
+        var numbers = new int[] {1,2,3,4,5};
+        var type = numbers.GetType();
+
+        do{
+            System.Console.WriteLine(type.FullName);
+            type = type.BaseType;
         }
-        return 0;
-    }
+        while(type!=null);
+    return 0;
+    }   
 }
